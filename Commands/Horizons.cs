4599201b8ed16a6917763bfd3c0509e1825ac40c
DeltaDizzy <EJBraswell@gmail.com>
2024-02-20 01:35:37 -0500

    suppress nullability warning

--- Commands/Horizons.cs
+++ Commands/Horizons.cs
@@ -10,8 +10,10 @@ namespace DiscordMarsSim.Commands
 {
     internal class OrbitModule : BaseCommandModule
     {
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         public Kinematics Kinematics { private get; set; }
-        
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+
         [Command("delay")]
         public async Task Delay(CommandContext context) => await context.RespondAsync($"The current one-way time delay is: {Kinematics.GetTimeDelay()}");
     }
